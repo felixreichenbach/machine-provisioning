16d49be74ab0f2864bc96d726b3e850cbff88097
Felix Reichenbach <38431269+felixreichenbach@users.noreply.github.com>
2024-04-25 19:03:24 +0200

    reformatted

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 """A simple sample script useful to learn about creating machines in app.viam.vom"""
+
 import asyncio
 import os
 from viam.rpc.dial import DialOptions
@@ -6,6 +7,7 @@ from viam.app.viam_client import ViamClient
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 async def connect():
     """Connect to app.viam.com"""
@@ -15,13 +17,15 @@ async def connect():
     # API_KEY=XXXXX
     # LOCATION_ID=XXXXX
     #
-    dial_options = DialOptions.with_api_key(os.getenv("API_KEY"), os.getenv("API_KEY_ID"))
+    dial_options = DialOptions.with_api_key(
+        os.getenv("API_KEY"), os.getenv("API_KEY_ID")
+    )
     return await ViamClient.create_from_dial_options(dial_options)
 
 
 async def main():
     """Main"""
-     # Make a Viam app client
+    # Make a Viam app client
     viam_client = await connect()
     # Instantiate a ProvisioningClient to run provisioning client API methods on
     app_client = viam_client.app_client
@@ -31,21 +35,29 @@ async def main():
     # for robot in robots:
     #     await app_client.delete_robot(robot.id)
     #     print("Robot deleted: " + robot.id)
-    
+
     # Prompt user for information to create new machines
     name = input("Enter the machine name prefix:")
     number = int(input("Enter the number of machines to create:"))
     # Create machines based upon user input
     robot_ids = []
     for i in range(number):
-        robot_ids.append(await app_client.new_robot(name+"_"+str(i),os.getenv("LOCATION_ID")))
-    # 
+        robot_ids.append(
+            await app_client.new_robot(name + "_" + str(i), os.getenv("LOCATION_ID"))
+        )
+    #
     for i in robot_ids:
         robot_parts = await app_client.get_robot_parts(i)
-        if (len(robot_parts) > 1):
+        if len(robot_parts) > 1:
             raise ValueError("Robot has more than one part, this is not supported")
-        f = open(i+".json", "w", encoding="utf-8")
-        f.write("{\"cloud\":{\"app_address\":\"https://app.viam.com:443\",\"id\":\""+robot_parts[0].id+"\",\"secret\":\""+robot_parts[0].secret+"\"}}")
+        f = open(i + ".json", "w", encoding="utf-8")
+        f.write(
+            '{"cloud":{"app_address":"https://app.viam.com:443","id":"'
+            + robot_parts[0].id
+            + '","secret":"'
+            + robot_parts[0].secret
+            + '"}}'
+        )
         f.close()
 
     # Don't forget to close the machine when you're done!
